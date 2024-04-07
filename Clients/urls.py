6a566161cf63c58603433e2501214c6259278f54
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2024-04-07 23:25:21 +0000

    req

--- Clients/urls.py
+++ Clients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import clients_list, clients_detail
+from .views import clients_list, clients_detail,clientsByEmail
 urlpatterns = [
     path('clients/register/', clients_list, name='client-list'),
-    path('clients/register/<int:pk>/', clients_detail, name='client-details')
+    path('clients/register/<int:pk>/', clients_detail, name='client-details'),
+    path('clients/register/<str:email>/', clientsByEmail, name='client-by-email'),
 ]
