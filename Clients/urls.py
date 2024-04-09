2c6ebbe9d5fdcb0ebbdbb1c74703728c2303987e
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2024-04-09 13:27:09 +0000

    login

--- Clients/urls.py
+++ Clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import clients_list, clients_detail,clientsByEmail
+from .views import clients_list, clients_detail,clientsByEmail, login
 urlpatterns = [
     path('clients/register/', clients_list, name='client-list'),
     path('clients/register/<int:pk>/', clients_detail, name='client-details'),
-    path('clients/check-email-exists/<str:email>/', clientsByEmail, name='client-by-email'),
+    path('clients/clientEmail/<str:email>/', clientsByEmail, name='client-by-email'),
+    path('clients/login/', login, name='login'),
 ]
