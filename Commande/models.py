2c628d05f558436064f911b5214458af64402da9
Hope <boukhatemabdelilah420@gmail.com>
2024-04-19 12:43:58 +0200

    ligncommande

--- Commande/models.py
+++ Commande/models.py
@@ -6,7 +6,8 @@ from Clients.models import Client
 class Commande(models.Model):
     id_comm = models.AutoField(primary_key=True)
     date_comm = models.DateField(auto_now=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default=0)
+    montant_total = models.DecimalField(max_digits=10000000, decimal_places=1000000, default=0)
 
     def __str__(self):
         return self.id_comm
--- Ligne_Commandes/models.py
+++ Ligne_Commandes/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from Commande.models import Commande
 from Produits.models import Produit
-from Factures.models import Facture
 
 class Ligne_Commande(models.Model):
     id_comm = models.ForeignKey(Commande, on_delete=models.CASCADE)
     id_prod = models.ForeignKey(Produit, on_delete=models.CASCADE)
-    num_facture = models.ForeignKey(Facture, on_delete=models.CASCADE)
     Quantite = models.IntegerField()
 
     class Meta:
        
-        unique_together = (('id_comm', 'id_prod', 'num_facture'),)
+        unique_together = (('id_comm', 'id_prod'),)
